057043f9bdba7dfc41d2082b1ae1894770878e16
Kenji Rikitake <kenji.rikitake@acm.org>
2012-04-20 14:08:50 +0900

    modified make-and-copy-tools.sh to include proper make targets

--- tqsllib-2.2/make-and-copy-tools.sh
+++ tqsllib-2.2/make-and-copy-tools.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 TARGET=/home/kenji/bin/tqsllib/bin
 
+make converter gen_crq station_loc load_cert
 cp -f converter gen_crq station_loc load_cert $TARGET
 mkdir $TARGET/.libs
 cp -fpr .libs/* $TARGET/.libs/
