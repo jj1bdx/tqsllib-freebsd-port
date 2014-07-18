3c2d55baf29b987b4bb4ac8ba259f90b045ed34d
Kenji Rikitake <kenji.rikitake@acm.org>
2014-07-18 11:10:50 +0900

    Update FreeBSD_build.sh with the install script

--- tqsllib-2.4/FreeBSD_build.sh
+++ tqsllib-2.4/FreeBSD_build.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+TARGET_DIR=/home/kenji/bin/tqsllib/bin/
+#
 cmake \
     -DCMAKE_BUILD_TYPE=Debug \
     -DBUILD_ALL_EXTRAPROGS=ON \
     -DTQSLLIB_STATIC=ON \
     .
 make
+#
+install -c -m 0755 gen_crq ${TARGET_DIR}
+install -c -m 0755 dumptqsldata ${TARGET_DIR}
+install -c -m 0755 converter ${TARGET_DIR}
+install -c -m 0755 station_loc ${TARGET_DIR}
+install -c -m 0755 load_cert ${TARGET_DIR}
